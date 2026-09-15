98ab95deea91f84a01b72b24aa6bb4fc217f5588
Keerthi-Varshitha <keerthibikkina98@gmail.com>
2026-09-15 11:27:49 +0530

    Time: 93 ms (86.10%) | Memory: 34.2 MB (84.39%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -3,12 +3,12 @@ class Solution:
         l=0
         window_elements=set()
         s=0
-        m=0
-        for r in range(len(nums)):
+        m=0                                                                                         
+        for r in range(len(nums)):                                                                
             while nums[r] in window_elements:
                 window_elements.remove(nums[l])
                 s-=nums[l]
-                l+=1
+                l+=1                
             window_elements.add(nums[r])
             s+=nums[r]
             if (r-l+1)==k:
